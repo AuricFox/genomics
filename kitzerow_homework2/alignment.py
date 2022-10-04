ef5c6851020fcee4632d6162664603923d4783a5
kitze012 <sskitzerow@gmail.com>
2022-10-04 08:41:55 -0500

    Adding string alignment

--- kitzerow_homework2/alignment.py
+++ kitzerow_homework2/alignment.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-class alignmant:
+class alignment:
     def __init__(self, seq1, seq2, gap_pen = -2, match_pen = -1):
         self.seq1 = seq1                        # Sequence 1
         self.seq2 = seq2                        # Sequence 2
         self.gap_pen = gap_pen                  # Gap penalty
         self.match_pen = match_pen              # Mismatch penalty
 
-        self.score = None                       # Alignment score
-        self.seq1_align = None                  # Alignment of sequence 1 with gaps added
-        self.seq2_align = None                  # Alignment of sequence 2 with gaps added
-        self.vis = None                         # Visualization of alignment (for text file)
+        self.score = 0                          # Alignment score
+        self.seq1_align = ""                    # Alignment of sequence 1 with gaps added
+        self.seq2_align = ""                    # Alignment of sequence 2 with gaps added
+        self.vis = ""                           # Visualization of alignment (for text file)
 
         self.s = None                           # Scoring matrix
         self.n = None                           # Neighbor matrix (tracks direction)
@@ -28,7 +28,7 @@ class alignmant:
         self.match_pen = match_pen
 
     # ----------------------------------------------------------------------------------------------------------------------
-    # Initializes scoring matrix
+    # Initializes scoring and neighbor matrices
     def build_matrix(self):
         col = len(self.seq1) + 1
         row = len(self.seq2) + 1
@@ -40,18 +40,19 @@ class alignmant:
         for i in range(col):
             k = (self.gap_pen * i)
             self.s[0, i] = k                                            # Adding row gap penalties
-            self.n[0, i] = (0,i-1)                                        # Adding row placeholders for neighbor matrix
+            self.n[0, i] = (0,i-1)                                      # Adding row placeholders for neighbor matrix
         
         k = 0
         for i in range(row):
             k = (self.gap_pen * i)
             self.s[i, 0] = k                                            # Adding column gap penalties
-            self.n[i, 0] = (i-1,0)                                        # Adding column placeholders for neighbor matrix
+            self.n[i, 0] = (i-1,0)                                      # Adding column placeholders for neighbor matrix
 
         self.n[0, 0] = None
+        self.align_sequence(1,1)
     
     # ----------------------------------------------------------------------------------------------------------------------
-    # Populates scoring matrix
+    # Populates neighbor and scoring matrices with corresponding placement scores and neighbors
     def align_sequence(self, col, row):
 
         if(len(seq1) <= (col-1) and len(seq2) > (row)):                 # End of row has been reached, move to next row
@@ -60,7 +61,7 @@ class alignmant:
             return
 
         k = 0
-        print("M: ", col," N: ", row)
+        # print("M: ", col," N: ", row)
         if(self.seq1[col-1] == self.seq2[row-1]):                       # Check for matches
             k = 1
         else:                                                           # No match, add penalty
@@ -84,15 +85,59 @@ class alignmant:
 
         self.align_sequence(col+1, row)                                 # Move to next element in row
 
+    # ----------------------------------------------------------------------------------------------------------------------
+    # Creates alignment strings for sequence 1, sequence 2, and alignment visualization. Also computes total alignment score.
+    def get_alignment(self):
+        row = len(self.seq2)
+        col = len(self.seq1)
+        pos = self.n[row][col]                                                                  # Start alignment here
+        
+        while(pos != None):
+            # print("Alignment: ", pos, " Row: ", row, " Col: ", col)
+
+            if(pos[0] == row-1 and pos[1] == col-1 and self.seq1[col-1] == self.seq2[row-1]):   # Diagonal alignment (match)
+                self.seq1_align = self.seq1[col-1] + self.seq1_align
+                self.seq2_align = self.seq2[row-1] + self.seq2_align
+                self.vis = "|" + self.vis
+                self.score += 1
+            
+            elif(pos[0] == row-1 and pos[1] == col-1 and self.seq1[col-1] != self.seq2[row-1]): # Diagonal alignment (mismatch)
+                self.seq1_align = self.seq1[col-1] + self.seq1_align
+                self.seq2_align = self.seq2[row-1] + self.seq2_align
+                self.vis = "X" + self.vis
+                self.score += self.match_pen
+
+            elif(pos[0] == row-1 and pos[1] == col):                                            # Verticle alignment (sequence 1 gap)
+                self.seq1_align = "_" + self.seq1_align
+                self.seq2_align = self.seq2[row-1] + self.seq2_align
+                self.vis = " " + self.vis
+                self.score += self.gap_pen
+
+            elif(row == pos[0] and pos[1] == col-1):                                            # Horizontal alignment (sequence 2 gap)
+                self.seq1_align = self.seq1[col-1] + self.seq1_align
+                self.seq2_align = "_" + self.seq2_align
+                self.vis = " " + self.vis
+                self.score += self.gap_pen
+
+            # Incrementing values to next neighbor
+            row = pos[0]
+            col = pos[1]
+            pos = self.n[row][col]
+        
+        return (self.seq1_align, self.seq2_align, self.vis, self.score)
+
 # ==========================================================================================================================
 # Testing
 if __name__ == "__main__":
-    seq1 = "ACGTA"
-    seq2 = "ACGT"
+    seq1 = "ACCGTA"
+    seq2 = "ACGTC"
 
-    a = alignmant(seq1, seq2)
+    a = alignment(seq1, seq2)
     #print(a.s)
     #print(a.n)
-    a.align_sequence(1,1)
     print(a.s)
     print(a.n)
+    a.get_alignment()
+    print(a.seq1_align)
+    print(a.vis)
+    print(a.seq2_align)
