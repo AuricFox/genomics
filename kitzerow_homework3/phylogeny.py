d52357a63e4c0fbeb500039b461f8fa5f91ca09d
kitze012 <sskitzerow@gmail.com>
2022-11-12 00:15:04 -0600

    nn

--- kitzerow_homework3/phylogeny.py
+++ kitzerow_homework3/phylogeny.py
@@ -22,7 +22,7 @@ class Phylogeny:
         self.seq = seq                  # List of taxa sequences
         self.header = header            # Correspoding markers for sequences
         self.dmatrix = None             # Distance matrix
-        self.edges = []                 # Edges of phylogenic tree
+        self.edges = {}                 # Edges of phylogenic tree
         self.tree = None                # Nodes of phylogenic tree
 
         self.build_Dmatrix()            # Build distance matrix
@@ -95,47 +95,61 @@ class Phylogeny:
         return qmatrix
 
     # ----------------------------------------------------------------------------------------------------------
-    # Joins neighboring sequences
-    # Returns a tuple containing (new dmatrix, new list of header info, edge A, edge B)
-    def join_neighbor(self, m, h, node):
-               
-        qm = self.q_matrix(m)                                                   # Build Q matrix
-        min = np.amin(qm)                                                       # Min value
-        loc = np.where(qm == min)                                               # Find mins
-        a, b = loc[0][0], loc[0][1]                                             # Index location of first min value
-        #print(m, "\n\n", qm)
+    # Calculates distances from each node
+    # Takes a dmatrix and two index values
+    # Returns calculated distance from a to u
+    def get_distance(self, m, a, b):
 
-        d_ab = m[a][b]
-        sum_a = sum(m[a][q] for q in range(m.shape[0]))                         # Summing distances in a
-        sum_b = sum(m[b][q] for q in range(m.shape[0]))                         # Summing distances in b
-        diff = sum_a - sum_b                                                    # Computing difference between a and b
+        if(a >= m.shape[0] or b >= m.shape[0]):
+            print("ERROR: INPUT OUT OF RANGE OF MATRIX!")
+            return
+
+        sum_a = sum(m[a][q] for q in range(m.shape[0]))         # Summing distances in a
+        sum_b = sum(m[b][q] for q in range(m.shape[0]))         # Summing distances in b
+        diff = sum_a - sum_b                                    # Computing difference between a and b
         s = 2 * (m.shape[0] - 2)
         #print("\nA: ", a, ", ", sum_a, " B: ", b, ", ", sum_b, " Diff: ", diff, " S: ", s)
 
-        d_au = (0.5)*(d_ab) + (diff / s)                                        # Calculating distance from a to u
-        d_bu = d_ab - d_au                                                      # Calculating distance from b to u
-        #print("Min: ", qm[a][b], " d_ab: ", d_ab, " d_au: ", d_au, " d_bu: ", d_bu)
+        return (0.5)*(m[a][b]) + (diff / s)                     # Calculating distance from a to u
+
+    # ----------------------------------------------------------------------------------------------------------
+    # Joins neighboring sequences
+    # Returns a tuple containing (new dmatrix, new list of header info, edge A, edge B)
+    def join_neighbor(self, m, h, node):
+
+        if(m.shape[0] != m.shape[1]):
+            print("ERROR: MATRIX MUST BE N X N!")
+            return
+               
+        qm = self.q_matrix(m)                               # Build Q matrix
+        min = np.amin(qm)                                   # Min value
+        loc = np.where(qm == min)                           # Find mins
+        a, b = loc[0][0], loc[0][1]                         # Index location of first min value
+        #print(m, "\n\n", qm)
+
+        d_au = self.get_distance(m, a, b)                   # Calculating distance from a to u
+        d_bu = m[a][b] - d_au                               # Calculating distance from b to u
+        #print("Min: ", qm[a][b], " d_ab: ", m[a][b], " d_au: ", d_au, " d_bu: ", d_bu)
 
         u = []
         for i in range(m.shape[0]):
             if(i == a): continue
-            d_u = (0.5)*(m[a][i] + m[i][b] - m[a][b])                           # Calaculating distances of each taxa to u
+            d_u = (0.5)*(m[a][i] + m[i][b] - m[a][b])       # Calaculating distances of each taxa to u
             u.append(d_u)
 
-        m = np.delete(m, [a], axis=0)                                           # Deleting row a
-        m = np.delete(m, [a], axis=1)                                           # Deleting column a
+        m = np.delete(m, [a], axis=0)                       # Deleting row a
+        m = np.delete(m, [a], axis=1)                       # Deleting column a
         #print("Trimming: \n", m, "\n")
 
-        m[a, 0:m.shape[0]] = u                                                  # Adding u distances to row (previously b)
-        m[0:m.shape[0], a] = u                                                  # Adding u distances to column (previously b)
+        m[a, 0:m.shape[0]] = u                              # Adding u distances to row (previously b)
+        m[0:m.shape[0], a] = u                              # Adding u distances to column (previously b)
         #print("Adding new distances: ", u, "\n", m)
 
-        edgeA = (node, h[a], d_au)                                            # Branch from a to u
-        edgeB = (node, h[b], d_bu)                                            # Branch from b to u
-        h[b] = node                                                             # Add new node to list
-        h.pop(a)                                                                # Remove extra label
+        self.edges[node] = {h[a]: d_au, h[b]: d_bu}         # Branch from a to u
+        h[b] = node                                         # Add new node to list
+        h.pop(a)                                            # Remove extra label
 
-        return (m, h, edgeA, edgeB)                                            # Return new dmatrix
+        return (m, h)                         # Return new dmatrix
 
     # ----------------------------------------------------------------------------------------------------------
     # Joins all the neighboring taxa together
@@ -147,21 +161,26 @@ class Phylogeny:
 
         while(m.shape[0] > 3):                              # Iterate thru taxa until there are only two nodes left
             data = self.join_neighbor(m, h, node)           # Join a neighboring taxa
-            self.edges.append([data[2], data[3]])           # Adding edges
-
             m = data[0]                                     # Updating matrix
             h = data[1]                                     # Updating header
             node += 1                                       # Updating new node label
 
-            print(m, '\n', data[2:4], '\n\n')
+            #print(m, '\n', data[2:4], '\n\n')
 
-        return
+        #print(self.q_matrix(m))
+        d_vw = self.get_distance(m, 0, 1)                   # Distance from v to w
+        d_wd = m[0][1] - d_vw                               # Distance from d to w
+        d_we = m[0][2] - d_vw                               # Distance from e to w
+
+        self.edges[node] = {h[0]:d_vw, h[1]:d_wd, h[2]:d_we}
+        #print("d_vw: ", d_vw, " d_wd: ", d_wd, " d_we: ", d_we)
+        #print("Edge V: ", edgeV, " Edge D: ", edgeD, " Edge E: ", edgeE)
 
     # ----------------------------------------------------------------------------------------------------------
     # Prints out values for debugging
     def debug(self):
-        #print(self.seq, self.header)
-        print(self.dmatrix)
+        print(self.header, "\n\n", self.dmatrix, "\n\n")
+        print(self.edges)
 
 # ==============================================================================================================
 # Retreives data from the fna file and returns a tuple containing a list of sequences and headers
@@ -185,7 +204,7 @@ def get_data(filename):
     return (seq_data, header_data)
 
 # ==============================================================================================================
-# Write distance matrix to text file
+# Write distance matrix to a text file
 def write_dmatrix(data, filename="./output/genetic-distances.txt"):
 
     with open(filename, 'w', newline='') as file:
@@ -195,6 +214,11 @@ def write_dmatrix(data, filename="./output/genetic-distances.txt"):
             s = '\t'.join([str(i) for i in data.dmatrix[x]])    # Join the row data together
             file.write(data.header[x] + '\t' + s + '\n')        # Write the header + data for each corresponding row
 
+# ==============================================================================================================
+# Write tree matrix to a text file
+def write_edges(data, filename="./output/edges.txt"):
+
+    return
 # ==============================================================================================================
 # Checking q-values 
 def check():
@@ -226,6 +250,7 @@ def main():
         #print(data.q_matrix(data.dmatrix))
         #stuff = data.join_neighbor(data.dmatrix, data.header, len(data.header) + 1)
         data.neighbor_joining()
+        data.debug()
         
     # ----------------------------------------------------------------------------------------------------------
     else:
